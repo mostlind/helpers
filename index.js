677a39cc29e37d87969f90b74fbf1d6cf8cc3560
Matt <mostlind@gmail.com>
2017-09-24 02:54:19 -0500

    Changes pipe implementation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const curry = fn => (fn.length === 1 ? fn : arg => curry(fn.bind(null, arg)));
-const pipe = (...fns) => arg => fns.reduce((acc, fn) => fn(acc), arg);
+const pipe = (fn, ...fns) => arg =>
+  fns.length ? pipe(...fns)(fn(arg)) : fn(arg);
 const map = fn => mappable => mappable.map(fn);
 const inspect = x => {
   console.log(x);
